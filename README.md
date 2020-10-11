3aa370c020abd4d182ac26549f277e93af20a35b
RashmiPant <rpant44@gmail.com>
2020-10-11 21:20:18 +0530

    Updated readme.md

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # googleTrends
+Get the top trending google searches for the day.
+
+## Usage
+```
+npm install
+npm run start
+```
+Go to <http://localhost:8000/googleTrends>
--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const parser = require("fast-xml-parser");
 const express = require("express");
 const app = express();
 
+app.get("/", (request,response)=> response.redirect("/googleTrends"));
+
 app.get("/googleTrends", (request, response) => {
     fetch("https://trends.google.com/trends/trendingsearches/daily/rss?geo=IN")
         .then(res => res.text())
@@ -16,16 +18,15 @@ app.get("/googleTrends", (request, response) => {
                     "link": newsTag.link,
                     "publishedDate": newsTag.pubDate,
                     "traffic": newsTag["ht:approx_traffic"]
-                })
+                });
             });
-            console.log(news);
             response.header("Content-Type",'application/json');
             response.send(JSON.stringify(news, null, 4));
         })
         .catch((error) => {
             console.log(error);
         });
-})
+});
 
 app.listen(8000, () => {
     console.log("Server running on port 8000");
